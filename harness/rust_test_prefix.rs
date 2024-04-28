f1ea716ffd26e9ff45339557302ded2c81df4c3c
Aria Beingessner <a.beingessner@gmail.com>
2024-04-27 23:41:47 -0400

    tagged unions and reference inputs

--- harness/rust_test_prefix.rs
+++ harness/rust_test_prefix.rs
@@ -26,13 +26,13 @@ pub struct FfiI128 {
 #[repr(C, align(16))]
 pub struct FfiU128 {
     low: u64,
-    high: u64,       
+    high: u64,
 }
 
 impl FfiI128 {
     fn new(val: i128) -> Self {
         Self {
-            low: val as u64 as i64, 
+            low: val as u64 as i64,
             high: (val as u128 >> 64) as u64 as i64,
         }
     }
@@ -41,8 +41,22 @@ impl FfiI128 {
 impl FfiU128 {
     fn new(val: u128) -> Self {
         Self {
-            low: val as u64, 
+            low: val as u64,
             high: (val as u128 >> 64) as u64,
         }
     }
+}
+
+unsafe fn write_field<T>(buffer: WriteBuffer, field: &T) {
+    WRITE_FIELD.unwrap()(
+        buffer,
+        field as *const T as *const u8,
+        core::mem::size_of_val(field) as u32
+    );
+}
+unsafe fn finished_val(buffer: WriteBuffer) {
+    FINISHED_VAL.unwrap()(buffer);
+}
+unsafe fn finished_func(inputs: WriteBuffer, outputs: WriteBuffer) {
+    FINISHED_FUNC.unwrap()(inputs, outputs);
 }

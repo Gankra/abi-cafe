a9a0b853aef3442864ab987ebcd695991495d2e4
Aria Beingessner <a.beingessner@gmail.com>
2024-06-24 00:02:06 -0400

    feat: reorg procgen

--- src/harness/read.rs
+++ src/harness/read.rs
@@ -25,7 +25,7 @@ pub fn read_tests(value_generator: ValueGeneratorKind) -> Result<Vec<Test>, Gene
             }
 
             // Otherwise, assume it's a test and parse it
-            let test = match read_test_manifest(&entry.path(), value_generator) {
+            let test = match read_test_manifest(entry.path().to_owned(), value_generator) {
                 Ok(test) => test,
                 Err(e) => {
                     eprintln!("test {:?}'s file couldn't be parsed {}", entry, e);
@@ -43,31 +43,37 @@ pub fn read_tests(value_generator: ValueGeneratorKind) -> Result<Vec<Test>, Gene
 
 /// Read a test .kdl file
 fn read_test_manifest(
-    test_file: &Path,
+    test_file: PathBuf,
     value_generator: ValueGeneratorKind,
 ) -> Result<Test, GenerateError> {
-    let file = File::open(test_file)?;
+    let (input, test_name) = if let Some(test_name) = filename(&test_file).strip_suffix(".procgen.kdl") {
+        let ty_def = read_file_to_string(&test_file)?;
+        let input = crate::procgen::procgen_test_for_ty_string(test_name, Some(&ty_def));
+        (input, test_name)
+    } else if let Some(test_name) = filename(&test_file).strip_suffix(".kdl") {
+        let input = read_file_to_string(&test_file)?;
+        (input, test_name)
+    } else {
+        return Err(GenerateError::Skipped);
+    };
+    let mut compiler = kdl_script::Compiler::new();
+    let types = compiler.compile_string(&test_file.to_string_lossy(), input)?;
+    let vals = Arc::new(ValueTree::new(&types, value_generator));
+    Ok(Test {
+        name: test_name.to_owned(),
+        types,
+        vals,
+    })
+}
+
+fn filename(file: &Path) -> &str {
+    file.file_name().and_then(|s| s.to_str()).unwrap_or("")
+}
+
+fn read_file_to_string(file: &Path) -> std::io::Result<String> {
+    let file = File::open(file)?;
     let mut reader = BufReader::new(file);
     let mut input = String::new();
     reader.read_to_string(&mut input)?;
-
-    let ext = test_file.extension().and_then(|s| s.to_str()).unwrap_or("");
-
-    if ext == "kdl" {
-        let mut compiler = kdl_script::Compiler::new();
-        let types = compiler.compile_string(&test_file.to_string_lossy(), input)?;
-        let vals = Arc::new(ValueTree::new(&types, value_generator));
-        Ok(Test {
-            name: test_file
-                .file_stem()
-                .expect("test had no filename")
-                .to_str()
-                .expect("test filename wasn't utf8")
-                .to_owned(),
-            types,
-            vals,
-        })
-    } else {
-        Err(GenerateError::Skipped)
-    }
+    Ok(input)
 }
--- src/main.rs
+++ src/main.rs
@@ -224,7 +224,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let cfg = cli::make_app();
     eprintln!("parsed cli!");
     // Before doing anything, regenerate the procgen tests, if needed.
-    procgen::procgen_tests(cfg.procgen_tests);
+    // procgen::procgen_tests(cfg.procgen_tests);
     eprintln!("generated tests!");
     let out_dir = init_dirs()?;
 
